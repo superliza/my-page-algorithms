f1b6959b586d3ad37f13cef9ce9e01db00b91d32
eli <eli_darko@hotmail.com>
2019-12-15 20:22:00 -0600

    programming in C partys candy and triangle surface

---
+++ actividades/algorithms-design/algorithmPartyCandy/partyCandy.c
@@ -0,0 +1,55 @@
+/*
+	Name: Dulces de la fiesta
+	Copyright: 2019
+	Author: Elizabeth Guerrero Torres
+	Date: 15/12/19 20:03
+	Description: Calcular cuántos dulces se consumieron en una fiesta y el gasto del consumo, tomando en cuenta un precio de $12.
+	
+	PSEUDOCÓDIGO 
+		0. Inicio 
+		1. Inicializar variables: 
+			1.1 eCandyLeftOver <- 0 
+			1.2 eTotalCandy <- 0 
+			1.3 eTotalCandyPurchased <- 0 
+			1.4 keCandyPrice <- 12 
+		2. Imprimir “¿Cuántos dulces compraste?” 
+		3. Leer eTotalCandyPurchased. 
+		4. Imprimir “¿Cuántos dulces sobraron?” 
+		5. Leer eCandyLeftOver. 
+		6. Calcular eTotalCandy <- eTotalCandyPurchased – eCandyLeftOver 
+		7. Calcular eTotalConsuption <- eTotalCandy * keCandyPrice 
+		8. Imprimir “El total de los dulces consumidos es ”, eTotalCandy “y el gasto del consumo es ”, eTotalConsuption
+		9. Fin
+*/
+
+//Biblioteca al preprocesador para funciones de entrada y salida principalmente.
+#include<stdio.h> //standard input-output header
+
+//Inicio del programa
+int main() {
+	//Inicializar variables
+	int eCandyLeftOver = 0; //variable que almacena los dulces que sobraron
+	int eTotalCandy = 0; //variable que almacena el total de los dulces que se consumieron
+	int eTotalCandyPurchased = 0; //variables que almacena los dulces que se compraron
+	int eTotalConsuption = 0; //variable que almacena el total del consumo en pesos.
+	
+	//Se inicializa la constante
+	const int keCandyPrice = 12; //constante del precio de los dulces
+	
+	//Se pide al usuario que ingrese el número de dulces que compró.
+	printf("¿uántos dulces compraste? ");
+	//Se guarda el número de dulces que compró.
+	scanf("%i", &eTotalCandyPurchased);
+	
+	//Se pide al usuario que ingrese los dulces que le sobraron en la fiesta
+	printf("¿Cuántos dulces sobraron? ");
+	//Se guarda el número de dulces que le sobraron.
+	scanf("%i", &eCandyLeftOver);
+	
+	//Se realiza la expresión para calcular el total de los dulces consumidos.
+	eTotalCandy = eTotalCandyPurchased - eCandyLeftOver;
+	//Se realiza otra expresión para calcular el total del consumo en pesos.
+	eTotalConsuption = eTotalCandy * keCandyPrice;
+	//Se imprime el consumo de sulces y el gasto.
+	printf("El total de los dulces consumidos es %i y el gasto del consumo es %i", eTotalCandy, eTotalConsuption);
+} //Fin del programa
---
+++ actividades/algorithms-design/algorithmTriangleSuperfice/triangleSuperfice.c
@@ -0,0 +1,47 @@
+/*
+	Name: Superficie del triángulo
+	Copyright: 2019
+	Author: Elizabeth Guerrero Torres
+	Date: 15/12/19 19:07
+	Description: De acuerdo a la base y altura de un triángulo, calcular la superficie.
+	
+	PSEUDOCÓDIGO 
+		0. Inicio 
+		1. Inicializar variables: 
+			1.1 fBase <- 0 
+			1.2 fHeight <- 0 
+			1.3 fSurface <- 0 
+		2. Imprimir “Dame el valor de la base” 
+		3. Leer fBase. 
+		4. Imprimir “Dame el valor de la altura” 
+		5. Leer fHeight 
+		6. Calcular fSurface <- (fBase * fHeight) / 2 
+		7. Imprimir “La superficie del triángulo es ” fSurface. 
+		8. Fin. 
+*/
+
+//Biblioteca al preprocesador para funciones de entrada y salida principalmente.
+#include<stdio.h> //standard input-output header
+
+//Inicio del programa
+int main() {
+	//Inicializar variables
+	float fBase = 0; //variable que almacena la base del triángulo.
+	float fHeight = 0; //variable que almacena la altura del triángulo.
+	float fSurface = 0; //variable que almacena la superficie del triángulo.
+	
+	//se le pide al usuario que ingrese el valor de la base.
+	printf("Dame el valor de la base: ");
+	//se lee el valor de la base.
+	scanf("%f", &fBase);
+	
+	//se le pide al usuario que ingrese el valor de la altura.
+	printf("Dame el valor de la altura: ");
+	//se lee el valor de la altura.
+	scanf("%f", &fHeight);
+	
+	//se realiza la expresión necesaria para calcular la superficie.
+	fSurface = (fBase * fHeight) / 2;
+	//se imprime la superficie del triángulo.
+	printf("La superficie del triángulo es: %.2f", fSurface);
+} // fin del programa
